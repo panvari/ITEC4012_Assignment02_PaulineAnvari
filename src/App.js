e7a72ba38de992d663a192041ce1f40b049c0531
panvari <paulineanvari@gmail.com>
2021-11-10 16:47:54 -0500

    creating navbar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import {Navbar} from './components/navbar';
+import {HomePage} from './components/pages/browse/index';
+import {SettingsPage} from './components/pages/settings/index';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route exact path='/'>
+            <HomePage></HomePage>
+          </Route>
+          <Route path='/new'>
+            <SettingsPage></SettingsPage>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
