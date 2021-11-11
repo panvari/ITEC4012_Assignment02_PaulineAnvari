abea9ebb040134be18da873c6c4f23a01924527b
panvari <paulineanvari@gmail.com>
2021-11-11 16:21:42 -0500

    updated navbar

--- src/components/pages/browse/index.jsx
+++ src/components/pages/browse/index.jsx
@@ -7,8 +7,6 @@ import logo from './logo.png';
 
 export const HomePage = () => {
     return(
-        <body>
-            <FeatureShow logo={logo} text="In 1980s Indiana, a group of young friends witness supernatural forces and secret government exploits." image={image}></FeatureShow>
-        </body>
+        <FeatureShow logo={logo} text="In 1980s Indiana, a group of young friends witness supernatural forces and secret government exploits." image={image}></FeatureShow>
     );
 }
